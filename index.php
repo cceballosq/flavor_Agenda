c3e3426ee60adbeb8f13718c8cbcd6d3e704016e
cceballosq <cceballosq@gmail.com>
2013-08-07 11:36:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<div id="cabecera" >
+  <div id="form_archivo">
+	<input type="text" name="name" id="name" value="" >
+	<div id="mod"></div>		
+</div>
+<div id="brinco"></div>
+<canvas id="prueba" width="500" height="20"></canvas>
+<div id="imprecion">	
+</div>
+</div>
+
+
+
+<div id="costado">
+	<div id="resultado"></div>
+<canvas id="barra_costado" width="300" height="20"></canvas><br>
+<div>
+	<?php echo $this->html->image("images.jpg","agenda") ?>
+</div>
+<div id="datos" style="padding:10px;">	
+</div>
+</div>
+
+
+
